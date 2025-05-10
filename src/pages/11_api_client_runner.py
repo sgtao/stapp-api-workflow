aa84150bad2a3606062177727adb8b6e8c3896b8
Shogo Ogami <sg.tao.so@gmail.com>
2025-05-10 17:29:00 +0900

    Add:update api_client_runner page for append get config_list

--- src/pages/11_api_client_runner.py
+++ src/pages/11_api_client_runner.py
@@ -1,4 +1,6 @@
 # api_client_runner.py
+import requests
+
 # import yaml
 
 import streamlit as st
@@ -9,7 +11,8 @@ APP_TITLE = "API Client Runner"
 
 
 def init_st_session_state():
-    pass
+    if "api_configs" not in st.session_state:
+        st.session_state.api_configs = []
 
 
 def sidebar():
@@ -19,6 +22,34 @@ def sidebar():
 def main():
     st.page_link("main.py", label="Back to Home", icon="🏠")
     st.title(f"🏃 {APP_TITLE}")
+
+    endpoint_hostname = st.text_input(
+        label="API Endpoint Hostname", value="localhost:3000"
+    )
+    endpoint_path = "api/v0/configs"
+    if st.button("Get API configs"):
+        endpoint = f"http://{endpoint_hostname}/{endpoint_path}"
+        st.session_state.api_configs = requests.get(endpoint).json()
+        st.json(
+            st.session_state.api_configs,
+            expanded=True,
+        )
+
+    if "result" in st.session_state.api_configs:
+        api_config_list = st.session_state.api_configs["result"]
+        config = st.selectbox(
+            label="Select a config",
+            options=api_config_list,
+        )
+
+        st.write("##### Config states")
+        with st.expander(
+            label="##### Config states",
+            expanded=False,
+            icon="📝",
+        ):
+            st.code(config)
+            st.write(config)
 
 
 if __name__ == "__main__":
